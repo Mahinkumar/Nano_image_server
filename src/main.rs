17f00dab3866b8d4b074fb24120ef0a35aa7f922
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-25 18:02:54 +0530

    Setup Image processing

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,23 @@ use axum::{
     Router,
 };
 
+use photon_rs::channels::alter_blue_channel;
+use photon_rs::native::{open_image,save_image};
+
 #[tokio::main]
 async fn main() {
+    // Image processing Test code
+    let mut img = open_image("./Test_images/in.jpg").expect("File should open");
+    alter_blue_channel(&mut img, 25_i16);
+    save_image(img, "./Test_images/out.jpg").expect("Unable to save");
 
     // build our application with a single route
     let app = Router::new().route("/images", get(|| async { "Hello from Nano Image Server!" }));
 
-    // run our app with hyper, listening globally on port 3000
+    // run our app with hyper, listening globally on port 8000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
-    
 }
+
+
+
