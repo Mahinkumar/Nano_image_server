b471838ab0ebbf0ff5c6428019f29c389bf2e524
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-26 09:47:25 +0530

    Setup basic Image Serving

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use axum::{
-    routing::get,
-    Router,
-};
+
+use axum::Router;
+
+use tower_http::services::ServeDir;
 
 use photon_rs::channels::alter_blue_channel;
 use photon_rs::native::{open_image,save_image};
@@ -12,9 +12,8 @@ async fn main() {
     let mut img = open_image("./Test_images/in.jpg").expect("File should open");
     alter_blue_channel(&mut img, 25_i16);
     save_image(img, "./Test_images/out.jpg").expect("Unable to save");
-
-    // build our application with a single route
-    let app = Router::new().route("/images", get(|| async { "Hello from Nano Image Server!" }));
+    
+    let app = Router::new().route_service("/images", ServeDir::new("images"));
 
     // run our app with hyper, listening globally on port 8000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
