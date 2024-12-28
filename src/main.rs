8bee394ce5cf18ca6aec6f666b6a30440a4471d6
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-28 11:03:37 +0530

    Redesign Serve Function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 
 
-use axum::extract::{Path, Query};
+use axum::extract::{Path, Query, Request};
 use axum::http::header;
 use axum::response::IntoResponse;
 use axum::routing::get;
-use axum::Router;
+use axum::{Router, ServiceExt};
 
 use photon_rs::transform::resize;
 
 use photon_rs::native::open_image;
 use serde::Deserialize;
 
+use std::net::SocketAddr;
+//use std::time::Instant; // For timing functions
+
 #[derive(Deserialize,Debug)]
 #[serde(default = "default_param")]
+
 struct ProcessParameters{
     resx: u32,
     resy: u32
@@ -22,29 +26,37 @@ fn default_param() -> ProcessParameters{
     ProcessParameters{ resx: 0 , resy: 0}
 }
 
+const ADDR: [u8; 4] = [127, 0, 0, 1];
+const PORT_HOST: u16 = 8000;
+
 #[tokio::main]
 async fn main() {
     // Image processing Test code
     let app = Router::new()
     .route("/image/:image", get(handler));
 
-    let addr = "0.0.0.0";
-    let port = 8000;
     println!("Nano Image Server Running...");
-    println!("Serving on http://localhost:{port}/image");
-
-    let final_addr = format!("{addr}:{port}");
-
+    println!("Serving on http://localhost:{PORT_HOST}/image");
 
     // run our app with hyper, listening globally on port 8000
-    let listener = tokio::net::TcpListener::bind(final_addr).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    tokio::spawn(async move { serve(app, PORT_HOST).await })
+        .await
+        .expect("Unable to Spawn Threads")
+}
+
+async fn serve(app:Router, port: u16) {
+    let addr = SocketAddr::from((ADDR, port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, ServiceExt::<Request>::into_make_service(app))
+        .await
+        .unwrap();
 }
 
 async fn handler(
     Path(image): Path<String>,
     Query(process_params): Query<ProcessParameters>,
 ) -> impl IntoResponse {
+    //let before = Instant::now();
     let input_path = format!("./images/{}", image);
 
     let img = match open_image(&input_path) {
@@ -65,9 +77,12 @@ async fn handler(
     if process_params.resx == 0{ no_resize = true };
     if process_params.resy == 0{ no_resize = true };
 
-    let final_image = if !no_resize {resize(&img, process_params.resx, process_params.resy, photon_rs::transform::SamplingFilter::Nearest)}else{img};
+    //println!("After Image open: {:.2?}", before.elapsed());
+
+    let final_image = if !no_resize {resize(&img, process_params.resx, process_params.resy, photon_rs::transform::SamplingFilter::CatmullRom)}else{img};
     let jpeg_bytes = final_image.get_bytes_webp();
     
+    //println!("After Process: {:.2?}", before.elapsed());
     // Convert Vec<u8> to axum::body::Body
     (
         [(header::CONTENT_TYPE, "image/jpeg")],
