36958d0945604f58954c8809e95bfdd281db4e40
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2025-01-05 18:32:07 +0530

    Setup New Web Console

---
+++ console/src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Console from "./lib/Console.svelte";
+    document.body.style.backgroundColor = '#292728';
+</script>
+
+<body class="w-screen h-screen bg-black">
+  <Console/>
+</body>
---
+++ console/src/lib/Console.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import {Pane} from 'tweakpane';
+  const pane = new Pane();
+
+  const PARAMS = {
+  percentage: 50,
+  theme: 'dark',
+  Parameter: 5,
+  Filter: 'blur',
+  color: '#ff0055',
+  };
+
+
+  
+  pane.addBinding(PARAMS, 'Filter');
+  pane.addBinding(PARAMS, 'color');
+  pane.addBinding(PARAMS, 'Parameter');
+
+
+  // `min` and `max`: slider
+  pane.addBinding(
+    PARAMS, 'percentage',
+    {min: 0, max: 100, step: 10}
+  );
+
+  // `options`: list
+  pane.addBinding(
+    PARAMS, 'theme',
+    {options: {Dark: 'dark', Light: 'light'}}
+  );
+</script>
+
