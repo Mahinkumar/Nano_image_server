b73cb0ede740c08d73377d4c0d23f771ba428780
mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2025-02-26 21:24:15 +0530

    update packages and documentation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-
-pub mod utils;
 pub mod cli;
 
 use axum::extract::{Path, Request};
@@ -16,42 +14,66 @@ use plugins::log;
 use std::net::SocketAddr;
 use tokio::net::TcpSocket;
 
+/// Loopback addr (Localhost)
 const ADDR: [u8; 4] = [127, 0, 0, 1];
 
 #[tokio::main]
 async fn main() {
-    
+    // Basic arguments are parsed here based on cli.rs file
+    // Check cli.rs for more information about the arguments
     let args = Args::parse();
 
+    // Runs only if the plugins feature is enabled
+    // Includes a suite of basic image processing methods
+    // check the lib.rs in /plugins/src directory for more info
     #[cfg(feature = "plugins")]
     log();
 
+    // Basic router with image request handler and a health check endpoint
+    // Todo: Include a security and rate limiting middleware before v0.6.0
     let app = Router::new()
         .route("/{image}", get(handler));
 
+    // Serve the app
     serve(app, args.port).await;
 }
 
-async fn serve(app: Router, port: u16) {
-    let addr = SocketAddr::from((ADDR, port));
 
+
+/// Serves the router in specified port.
+/// Anything related to sockets and serving must be implemented here
+/// Includes socket configurations including SO_SNDBUF, SO_RCVBUF and TCP_NODELAY.
+/// The socket backlog is also defined here.
+async fn serve(app: Router, port: u16) {
+    // Address and socket definitions
+    let addr = SocketAddr::from((ADDR, port));
     let socket = TcpSocket::new_v4().unwrap();
 
+    //socket configurations
     socket.set_send_buffer_size(524_288).unwrap();
     socket.set_recv_buffer_size(524_288).unwrap();
     socket.set_nodelay(true).unwrap();
-
     socket.bind(addr).unwrap();
+
+    /*
+    Socket backlog configuration.
+
+    Defines the maximum number of pending connections that are 
+    queued by the operating system at any moment*/
     let listener = socket.listen(2048).unwrap();
 
+    // Serve the app service to specified listener
     axum::serve(listener, ServiceExt::<Request>::into_make_service(app))
         .await
         .unwrap();
 }
 
+
+/// Standard handler for the Image Server
 async fn handler(
     Path(image): Path<String>,
 ) -> impl IntoResponse {
+
     // Due to rewrite the functions are incomplete here. 
 
     return (
