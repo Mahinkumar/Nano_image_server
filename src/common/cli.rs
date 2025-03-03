fec4da785fa4ab955bb9c3e53be42664ec1b65c2
mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2025-03-03 09:27:05 +0530

    Integrate cli args

--- src/common/cli.rs
+++ src/common/cli.rs
@@ -5,7 +5,6 @@ use clap::Parser;
 pub struct Args {
     // --------------------------------------------
     // Port and URL Configurations
-    
     /// Defines the port the app is hosted on
     #[arg(long, short, default_value_t = 8000)]
     pub port: u16,
@@ -16,26 +15,23 @@ pub struct Args {
 
     // --------------------------------------------
     // Security configuration
-
     /// Enables TLS. Requires certificates in cert folder
     #[arg(long, short, default_value_t = false)]
     tls: bool,
 
-    /// Enables Security Middleware. Enabled by default, Disabling bypasses the middleware entirely. 
+    /// Enables Security Middleware. Enabled by default, Disabling bypasses the middleware entirely.
     #[arg(long, short, default_value_t = false)]
     security_middleware: bool,
 
     // --------------------------------------------
     // Plugins support
-    
     /// Enable Plugin support
     #[arg(long, short, default_value_t = false)]
     allow_plugins: bool,
 
     // --------------------------------------------
     // Caching configuration
-
-    /// Toggle for Caching. Set to false by default and allows caching. 
+    /// Toggle for Caching. Set to false by default and allows caching.
     #[arg(long, short, default_value_t = false)]
     no_cache: bool,
 
@@ -46,6 +42,5 @@ pub struct Args {
     /// Limit for storage based caching in Megabytes.
     #[arg(long, short, default_value_t = 1024*4)]
     cache_limit: u32,
-
     // --------------------------------------------
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ pub mod security;
 pub mod storage;
 
 use axum;
+use clap::Parser;
+use cli::Args;
+use common::cli;
 
 #[tokio::main]
 async fn main() {
+    let _args = Args::parse();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, handlers::router::main_router()).await.unwrap();
+    axum::serve(listener, handlers::router::main_router())
+        .await
+        .unwrap();
 }
