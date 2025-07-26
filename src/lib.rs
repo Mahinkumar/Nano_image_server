ca5d91e8c493ef958dddbd17c21faeceba004e46
mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2025-07-27 00:18:39 +0530

    Update default feature set

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #[cfg(feature = "processing")]
 pub mod processing;
 
+#[cfg(feature = "processing")]
 pub mod transform;
 
 pub mod server;
+
+#[cfg(feature = "processing")]
 pub mod utils;
 
 pub const ADDR: [u8; 4] = [127, 0, 0, 1];
